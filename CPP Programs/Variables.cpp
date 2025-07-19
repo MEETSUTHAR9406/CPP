a6e5942a9e52c5f109081f97864797580f237559
MEETSUTHAR9406 <MEETSUTHAR9406@users.noreply.github.com>
2025-07-19 20:50:09 +0530

    Learned about C++ Primitive Data types

---
+++ CPP Programs/Variables.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    // Primitive Data Types 
+
+    int age = 20;
+    char grade = 'A';
+    float PI = 3.14f;
+    bool isSafe = false;
+    double price = 100.99;
+
+    cout << "Age is " << age << endl;
+    cout << "Size of int is " << sizeof(age) << endl;
+
+    cout << "Grade is " << grade << endl;
+    cout << "Size of char is " << sizeof(grade) << endl;
+
+    cout << "PI is " << PI << endl;
+    cout << "Size of float is " << sizeof(float) << endl;
+
+    cout << "Is safe?? " << isSafe << endl;
+    cout << "Size of bool is " << sizeof(bool) << endl; 
+
+    cout << "Price is " << price << endl;
+    cout << "Size of double is " << sizeof(double) << endl;
+
+    return 0;
+}
