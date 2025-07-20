864ab76bcc94f308fd12e6d7de6cb08e8cce924a
MEETSUTHAR9406 <MEETSUTHAR9406@users.noreply.github.com>
2025-07-20 19:14:02 +0530

    Learned about Relational Operator in CPP

---
+++ CPP Programs/RelationalOpeartors.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    cout << (3 < 5) << endl; // True --> 1
+    cout << (3 > 5) << endl; // False --> 0
+    cout << (3 <= 3) << endl; // True --> 1
+    cout << (3 == 5) << endl; // False --> 0
+    cout << (3 != 5) << endl; // True --> 1
+
+    return 0;
+}
