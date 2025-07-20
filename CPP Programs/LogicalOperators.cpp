ad06d94491f5e5c8aaf4970b52e9fa43b844f6e9
MEETSUTHAR9406 <MEETSUTHAR9406@users.noreply.github.com>
2025-07-20 19:38:53 +0530

    Learned about Logical Operators in CPP

---
+++ CPP Programs/LogicalOperators.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    // Logical Operators
+
+    cout << !(3 > 1) << endl; // Logical NOT 
+
+    cout << ((3 < 1) || (3 < 5)) << endl; // Logical OR
+
+    cout << ((3 > 1) && (3 > 4)) << endl; // Logical AND
+
+    return 0;
+}
