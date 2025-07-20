dea1647fd1a9a9fc58508fa77801a35cc90ba545
MEETSUTHAR9406 <MEETSUTHAR9406@users.noreply.github.com>
2025-07-20 18:09:39 +0530

    Tried taking some inputs in CPP

---
+++ CPP Programs/Inputs.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    int age;
+    int price;
+
+    cout << "Enter your age: ";
+    cin >> age;  
+
+    cout << "Enter the price: ";
+    cin >> price;
+
+    cout << "Your age is: " << age << endl;
+    cout << "The price is: " << price << endl;
+    
+    return 0;
+}
