72f66e7cef59b29f43e8cd8a722832ad73df995a
MEETSUTHAR9406 <MEETSUTHAR9406@users.noreply.github.com>
2025-07-22 18:47:42 +0530

    Learned about Do-While loops

---
+++ CPP Programs/DoWhile.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    int num = 10;
+    int i = 1;
+
+    do {
+        cout << i << " ";
+        i++;
+    } while (i <= num);
+
+    cout << endl;
+
+    return 0;
+}
