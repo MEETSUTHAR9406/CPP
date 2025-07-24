c1709579b9637905487fd978c618f99323527a8e
MEETSUTHAR9406 <MEETSUTHAR9406@users.noreply.github.com>
2025-07-24 22:29:05 +0530

    Updated file

--- CPP Programs/ReverseTrianglePattern.cpp
+++ CPP Programs/ReverseTrianglePattern.cpp
@@ -11,7 +11,6 @@ int main() {
     for(int i = 0; i < num; i++) {
         for(int j = i + 1; j > 0; j--) {
             cout << j << " ";
-            // num--;
         }
         cout << endl;
     }
