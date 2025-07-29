508e5bbb434bd0cbf2aba66e9a2cf91595647163
MEETSUTHAR9406 <MEETSUTHAR9406@users.noreply.github.com>
2025-07-29 23:43:20 +0530

    Implemented program to find Binary number from the Decimal number

---
+++ CPP Programs/DectoBin.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+using namespace std;
+
+int DectoBinary(int num) {
+    int ans = 0, pow = 1;
+
+    while (num > 0) {
+        int digit = num % 2;
+        num /= 2;
+        ans += (digit * pow);
+        pow *= 10;
+    }
+    return ans;
+}
+
+int main() {
+
+    int num, ans;
+
+    cout << "Enter the number: ";
+    cin >> num;
+
+    for(int i = 1; i <= 10; i++) {
+        cout << "Decimal to binary of " << i << " is " << DectoBinary(i) << endl;
+    }
+
+    // DectoBinary(num);
+    // cout << "Decimal to binary of " << num << " is " << DectoBinary(num) << endl;
+
+
+    return 0;
+}
