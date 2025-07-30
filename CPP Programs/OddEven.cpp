fd0c855e4c966af362e186a23a8e51e0b75ba1dc
MEETSUTHAR9406 <MEETSUTHAR9406@users.noreply.github.com>
2025-07-30 23:44:30 +0530

    Writing comments

--- CPP Programs/OddEven.cpp
+++ CPP Programs/OddEven.cpp
@@ -5,14 +5,15 @@ int main() {
 
     int num;
 
+    // Takes user input
     cout << "Enter the number: ";
     cin >> num;
 
+    // Checks if number is divisible by two or not.
     if(num % 2 == 0) {
-        cout << num << " is a even number."; 
+        cout << num << " is a even number."; // if the remainder of a certain value divided by 2 would be zero, then the condition would be fulfilled
     } else {
-        cout << num << " is a odd number.";
+        cout << num << " is a odd number."; // else it will print odd values.
     }
-
     return 0;
 }
