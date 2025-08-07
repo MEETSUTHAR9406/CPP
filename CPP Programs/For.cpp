70383c5f9857ac76f9fc8ab90f986a7dbda0dcb5
MEETSUTHAR9406 <MEETSUTHAR9406@users.noreply.github.com>
2025-08-07 23:17:49 +0530

    Updated For.cpp (A concept of FOR loop).

--- CPP Programs/For.cpp
+++ CPP Programs/For.cpp
@@ -8,6 +8,7 @@ int main() {
     cout << "Enter the number: ";
     cin >> num;
 
+    // We will mostly use for loops in most of the tasks, cause it is easier and faster.
     for (int i = 1; i <= num; i += 2) {
         cout << i << " ";
     }
