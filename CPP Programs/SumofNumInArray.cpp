290107dff05d781e7a79737cf38a5367814ac2f9
MEETSUTHAR9406 <MEETSUTHAR9406@users.noreply.github.com>
2025-08-09 22:32:33 +0530

    Added comments for better readability/look.

--- CPP Programs/SumofNumInArray.cpp
+++ CPP Programs/SumofNumInArray.cpp
@@ -8,19 +8,22 @@ int main() {
     cout << "How many elementts you want to store? ";
     cin >> num;
 
-    int arr[num];
-    int size = sizeof(arr) / sizeof(arr[0]);
+    int arr[num]; // Initializing array of 'num' values.
+    int size = sizeof(arr) / sizeof(arr[0]); // To find size of the array. 
 
+    // Taking input of 'num' of array.
     for(int i = 0; i < size; i++) {
         cout << "arr[" << i << "]: ";
         cin >> arr[i];
     }
 
     int sum = 0;
+    // Adding numbers from index 0 to the end of the array.
     for(int i = 0; i < size; i++) {
         sum += arr[i];
     }
 
+    // Printing the sum of the array.
     cout << "The sum of numbers in the array is " << sum << endl;
 
     return 0;
