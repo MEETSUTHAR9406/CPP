d30a54b6cc924336914085b01d1d1954ed1abf74
MEETSUTHAR9406 <MEETSUTHAR9406@users.noreply.github.com>
2025-08-11 23:27:44 +0530

    Learned basic things about vectors

---
+++ CPP Programs/Vectors.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+// #include <bits/c++.h>
+#include <vector>
+using namespace std;
+
+int main() {
+
+    // vector<int> vec;
+    // cout << vec[0];
+
+    vector<int> vec = {1, 2, 3};
+    cout << vec[0] << endl;
+
+    return 0;
+}
