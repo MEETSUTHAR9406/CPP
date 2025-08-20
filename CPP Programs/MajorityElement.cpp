c969708e13f38faf1f88de142f93760443ec9a52
MEETSUTHAR9406 <MEETSUTHAR9406@users.noreply.github.com>
2025-08-20 23:10:37 +0530

    Learned and implemented about Majority Elment ni the array

--- CPP Programs/MajorityElement.cpp
+++ CPP Programs/MajorityElement.cpp
@@ -2,24 +2,32 @@
 #include <vector>
 using namespace std;
 
-// vector<int> MajSum(vector<int> nums) {
-//     vector<int> ans;
+class Solution {
+public:
+    int majorityElement(vector<int>& nums) {
+        int n = nums.size();  // fixed semicolon
+        for (int val : nums) {
+            int freq = 0;
 
-//     for(int i = 0; i < )
-// }
-
-int main() {
-
-    vector<int> nums = {2, 7, 11, 15};
-
-    for(int val : nums) {
-        int freaq = 0;
-        for(int el : nums) {
-            if(el == val) {
-                freaq++;
+            for (int el : nums) {
+                if (el == val) {
+                    freq++;
+                }
+            }
+            if (freq > n / 2) {
+                return val;
             }
         }
+        return -1;
     }
+};
+
+int main() {
+    Solution obj;
+    vector<int> nums = {2, 2, 1, 1, 1, 2, 2};
+
+    int ans = obj.majorityElement(nums);
+    cout << "Majority Element: " << ans << endl;
 
     return 0;
 }
