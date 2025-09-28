670b594b6dca23f16a0412b9d2d440242cd4f078
MEETSUTHAR9406 <MEETSUTHAR9406@users.noreply.github.com>
2025-09-28 22:38:10 +0530

    Implemented program of pairSum using simple method.

--- CPP Programs/PairSum.cpp
+++ CPP Programs/PairSum.cpp
@@ -2,35 +2,35 @@
 #include <vector>
 using namespace std;
 
-vector<int> pairSum(vector<int> nums, int target) {
+vector<int> pairSum(vector<int> arr, int target) {
     vector<int> ans;
-    int n = nums.size();
+    int n = arr.size();
 
-    int i = 0, j = n - 1;
-
-    while(i < j) {
-        int pairSum = nums[i] + nums[j];
-
-        if(pairSum > target) {
-            j--;
-        } else if(pairSum < target) {
-            i++;
-        } else {
-            ans.push_back(i);
-            ans.push_back(j);
-            return ans;
+    for(int i = 0; i < n; i++) {
+        for(int j = i + 1; j < n; j++) {
+            if(arr[i] + arr[j] == target) {
+                ans.push_back(i);
+                ans.push_back(j);
+                return ans;
+            }
         }
     }
-
     return ans;
 }
 
 int main() {
+    vector<int> arr(4);
+    int target;
 
-    vector<int> nums = {2, 7, 11, 15};
-    int target = 26; 
+    cout << "Enter your target value: ";
+    cin >> target;
 
-    vector<int> ans = pairSum(nums, target);
+    for(int i = 0; i < arr.size(); i++) {
+        cout << "arr[" << i << "]: ";
+        cin >> arr[i];
+    }
+
+    vector<int> ans = pairSum(arr, target);
     cout << ans[0] << ", " << ans[1] << endl;
 
     return 0;
