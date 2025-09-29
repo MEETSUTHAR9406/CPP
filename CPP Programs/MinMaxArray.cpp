c59b6bc52fe09962f0197a277b22713db2d3ff7f
MEETSUTHAR9406 <MEETSUTHAR9406@users.noreply.github.com>
2025-09-29 23:35:28 +0530

    Optimized program to find min & max in an array.

--- CPP Programs/MinMaxArray.cpp
+++ CPP Programs/MinMaxArray.cpp
@@ -1,40 +1,26 @@
 #include <iostream>
+#include <vector>
 using namespace std;
 
 int main() {
 
-    int arr[5];
-    int size = sizeof(arr) / sizeof(arr[0]);
+    vector<int> arr(10);
 
-    for(int i = 0; i < size; i++) {
-        cout << "Enter value at index " << i + 1 << " ";
+    for(int i = 0; i < arr.size(); i++) {
+        cout << "arr[" << i << "]: ";
         cin >> arr[i];
     }
 
-    int max = arr[0]; // int max = __WINT_MIN__
-    int min = arr[size - 1]; // int min = __INT_MAX__
+    int minVal = arr[0];
+    int maxVal = arr[0];
 
-    cout << "Your array is " << endl;
-    for(int i = 0; i < size; i++) {
-        cout << arr[i] << " ";
+    for(int i = 1; i < arr.size(); i++) {
+        if(arr[i] < minVal) minVal = arr[i];
+        if(arr[i] > maxVal) maxVal = arr[i];
     }
 
-    for(int i = 0; i < size; i++) {
-        if(arr[i] >= max) {
-            max = arr[i];
-        }
-    }
-
-    for(int i = size - 1; i >= 0; i--) {
-        if(arr[i] <= min) {
-            min = arr[i];
-        }
-    }
-
-    cout << endl;
-
-    cout << "Maximum element in the array is " << max << endl;
-    cout << "Minimum element in the array is " << min << endl;
+    cout << "Minimum: " << minVal << endl;
+    cout << "Maximum: " << maxVal << endl;
 
     return 0;
 }
