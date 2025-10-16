5bfe970d1e9d7fd2738541b6d6b35fb28d528364
MEETSUTHAR9406 <MEETSUTHAR9406@users.noreply.github.com>
2025-10-16 23:58:48 +0530

    Some changes in DectoBin, and a new program of Pow_X_N

--- CPP Programs/Pow_X_N.cpp
+++ CPP Programs/Pow_X_N.cpp
@@ -1,10 +1,10 @@
 #include <iostream>
 using namespace std;
 
-int DectoBinary(int num) {
+int toBin(int num) {
     int ans = 0, pow = 1;
 
-    while (num > 0) {
+    while(num > 0) {
         int digit = num % 2;
         num /= 2;
         ans += (digit * pow);
@@ -20,13 +20,8 @@ int main() {
     cout << "Enter the number: ";
     cin >> num;
 
-    for(int i = 1; i <= 10; i++) {
-        cout << "Decimal to binary of " << i << " is " << DectoBinary(i) << endl;
-    }
-
-    // ans = DectoBinary(num);
-    // cout << "Decimal to binary of " << num << " is " << ans << endl;
-
+    ans = toBin(num);
+    cout << "Decimal to binary of " << num << " is " << ans << endl;
 
     return 0;
 }
